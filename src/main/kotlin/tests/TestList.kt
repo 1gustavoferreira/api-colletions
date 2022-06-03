1131749bf0b325d1c5c8991e15ea7cb095670347
soldng <gustavoferreira0gu@gmail.com>
2022-06-03 12:57:48 -0300

    Test Listas = separar tipos por grupos,colocar em ordem por strings

---
+++ src/main/kotlin/tests/TestList.kt
@@ -0,0 +1,42 @@
+package tests
+
+fun main() {
+    val gustavo = Funcionario("joao", 3000.0, "CLT")
+    val ferreira = Funcionario("ferreira", 5000.0,"PJ")
+    val aline = Funcionario("Aline", 2000.0, "CLT")
+
+    val funcionarios = listOf(gustavo, ferreira, aline)
+
+    //O it AGORA VIRA O FUNCIONARIO
+    funcionarios.forEach {
+        println(it)
+    }
+    //SEPARA OS TIPOS DE CONTRATAÇÃO
+    println("----------------------")
+    funcionarios.groupBy { it.tipoContratacao }.forEach{ println(it)}
+    println("groupby")
+
+    println("----------------------")
+    println(funcionarios.find { it.nome == "Aline" })
+
+    //ORDENOU OS NOMES EM ORDEM
+    println("----------------------")
+    funcionarios.sortedBy { it.nome }.forEach {
+        println(it)
+    }
+    println("----------------------")
+
+}
+
+data class Funcionario(
+    val nome: String,
+    val salario: Double,
+    val tipoContratacao: String
+) {
+    //PARA FAZER O RETORNO
+    override fun toString(): String =
+        """
+            nome: $nome
+            salario: $salario
+        """.trimIndent()
+}
